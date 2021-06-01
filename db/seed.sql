6667b8447e657d9c2d7b5786ff6f940587caeab3
jtread1215 <77247419+jtread1215@users.noreply.github.com>
2021-06-01 17:37:46 -0400

    setup

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 110000, 1),
+    ('Salesperson', 90000, 1),
+    ('Lead Engineer', 170000, 2),
+    ('Software Engineer', 140000, 2),
+    ('Account Manager', 150000, 3),
+    ('Human Resources Manager', 120000, 5),
+    ('Accountant', 130000, 3),
+    ('Legal Team Lead', 230000, 4),
+    ('Lawyer', 200000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Ricky', 'Vaugh', 2, NULL),
+    ('Roger', 'Dorn', 1, NULL),
+    ('Rachel', 'Phelps', 4, NULL),
+    ('Jake', 'Taylor', 1, 1),
+    ('Pedro', 'Cerrano', 3, NULL),
+    ('Lou', 'Brown', 4, NULL),
+    ('Harry', 'Doyle', 1, NULL),
+    ('Suzanne', 'Dorn', 3, 5),
+    ('Lynn', 'Wells', 3, NULL),
+    ('Willie', 'Mays', 8, 7);
+
